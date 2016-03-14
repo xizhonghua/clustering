bd75f95bb207ffabccb2fc0b06784f2603fb77e6
Zhonghua Xi <xizhonghua@gmail.com>
2016-03-13 22:06:16 -0400

    update exp

---
+++ examples/kmeans.cpp
@@ -0,0 +1,27 @@
+/*
+ * kmeans.cpp
+ *
+ *  Created on: Mar 13, 2016
+ *      Author: zxi
+ */
+
+/*
+ * kmeans.cpp
+ *
+ *  Created on: Mar 12, 2016
+ *      Author: zxi
+ */
+
+#include <iostream>
+#include "libclustering/Clustering.h"
+using namespace masc::clustering;
+
+int main(int argc, char** argv) {
+  auto X = Eigen::MatrixXd::Random(100, 3);
+  KMeans kmeans(4);
+  auto labels = kmeans.fit_predit(X);
+
+  for (int i = 0; i < labels.rows(); ++i)
+    std::cout << X.row(i) << " " << labels(i) << std::endl;
+}
+
